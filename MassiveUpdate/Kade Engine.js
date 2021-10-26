1b4420b30450fedd329b78bbabe09cbe2c9fef6b
DerpySlurrpe <jahiem41@outlook.com>
2021-10-26 15:42:01 +0000

    .

--- MassiveUpdate/Kade Engine.js
+++ MassiveUpdate/Kade Engine.js
@@ -67935,7 +67935,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 826141;
+	this.version = 49810;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
