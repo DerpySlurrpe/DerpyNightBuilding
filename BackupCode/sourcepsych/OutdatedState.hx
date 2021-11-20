35e59934ad975b607aa6137b4c2841a75167c763
DerpySlurrpe <jahiem41@outlook.com>
2021-11-20 22:48:36 +0000

    .

--- BackupCode/sourcepsych/OutdatedState.hx
+++ BackupCode/sourcepsych/OutdatedState.hx
@@ -24,11 +24,11 @@ class OutdatedState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Sup bro, looks like you're running an   \n
-			outdated version of Psych Engine (" + MainMenuState.psychEngineVersion + "),\n
+			"Yo Buddy looks like you have a   \n
+			outdated version of Slush Engine (" + MainMenuState.slushiEngineVersion + "),\n
 			please update to " + TitleState.updateVersion + "!\n
 			\n
-			Thank you for using the Engine!",
+			Apprecate You Using The Engine!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
